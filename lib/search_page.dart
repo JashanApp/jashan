b62a2bbcabf7ab978f3ca0f4b5479d02305346a4
Arham4 <arhamjsiddiqui@gmail.com>
2019-07-04 12:48:18 -0500

    Search functionality

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:jashan/user.dart';
 
 class SearchPage extends StatefulWidget {
@@ -17,6 +21,17 @@ class _SearchPageState extends State<SearchPage> {
   Widget _appBarTitle = Container();
   bool _searching = false;
 
+  List<_SearchItem> _searchItems = List<_SearchItem>();
+
+  @override
+  void initState() {
+    super.initState();
+    _searchItems.add(_SearchItem(
+        thumbnail: Image.asset('assets/images/jashan_black.png'),
+        title: 'Jashan',
+        artist: 'Rhythm Garg, Arham Siddiqui'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,6 +47,13 @@ class _SearchPageState extends State<SearchPage> {
                 _searching = !_searching;
                 if (_searching) {
                   _appBarTitle = TextField(
+                    autocorrect: false,
+                    onChanged: (searchQuery) {
+                      if (searchQuery.length > 2) {
+                        _searchItems.clear();
+                        _repopulateSearchList(searchQuery);
+                      }
+                    },
                     style: TextStyle(
                       color: Colors.white,
                     ),
@@ -52,7 +74,96 @@ class _SearchPageState extends State<SearchPage> {
           )
         ],
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: _searchItems.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return SizedBox(
+              height: 10,
+            );
+          }
+          return _searchItems[index - 1];
+        },
+      ),
+    );
+  }
+
+  void _repopulateSearchList(String searchQuery) async {
+    Response queryResponse = await get(
+        'https://api.spotify.com/v1/search?q="$searchQuery"&type=track',
+        headers: {'Authorization': 'Bearer ${widget.user.accessToken}'});
+    List items = json.decode(queryResponse.body)['tracks']['items'];
+    setState(() {
+      items.forEach((albums) {
+        Map album = albums['album'];
+        String id = album['id'];
+        String imageUrl = album['images'][0]['url'];
+        String name = album['name'];
+        const int CAP = 37;
+        name =
+            '${name.substring(0, min(name.length, CAP))}${name.length > CAP ? '...' : ''}';
+        List artists = album['artists'];
+        String artistsString = artists[0]['name'];
+        for (int i = 1; i < artists.length; i++) {
+          artistsString += ', ${artists[i]['name']}';
+        }
+        _searchItems.add(
+          _SearchItem(
+              thumbnail: Image.network(imageUrl),
+              title: name,
+              artist: artistsString),
+        );
+      });
+    });
+  }
+}
+
+class _SearchItem extends StatelessWidget {
+  Image thumbnail;
+  String title;
+  String artist;
+
+  _SearchItem(
+      {@required this.thumbnail, @required this.title, @required this.artist});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 15),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 50,
+            height: 50,
+            child: thumbnail,
+          ),
+          Container(
+            height: 50,
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 16),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(artist,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                        fontSize: 13)),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
