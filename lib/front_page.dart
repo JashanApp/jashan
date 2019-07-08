fe366c2d3be5539c328b93bcd33a1fb519f17fe3
Arham4 <arhamjsiddiqui@gmail.com>
2019-07-07 19:31:05 -0500

    Change front page colors to be peach and orange

--- lib/front_page.dart
+++ lib/front_page.dart
@@ -12,9 +12,9 @@ class FrontPageViewer extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [Color.fromRGBO(0, 71, 255, 1), Color.fromRGBO(255, 117, 0, 1)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color.fromRGBO(255, 117, 0, 1), Color.fromRGBO(224, 178, 132, 1)],
           ),
         ),
         child: frontPage,
