ed97c72b991b7e7f9104c707971ef0f28e3925d9
Arham4 <arhamjsiddiqui@gmail.com>
2019-08-12 00:39:48 -0500

    Remove unneeded widget in item view

--- lib/views/playlist_item_view.dart
+++ lib/views/playlist_item_view.dart
@@ -67,60 +67,58 @@ class PlaylistQueueItemCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 3.0),
-      child: InkWell(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 45,
-              height: 45,
-              child: data.thumbnail,
-            ),
-            SizedBox(
-              width: 15,
-            ),
-            Container(
-              width: 120,
-              child: Text(
-                "${data.title.substring(0, min(data.title.length, 8))}${data.title.length > 8 ? "..." : ""}",
-                style: TextStyle(fontSize: 22, color: Colors.orange),
-              ),
-            ),
-            SizedBox(
-              width: 50,
-            ),
-            Text(
-              "${data.upvotes}",
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: 45,
+            height: 45,
+            child: data.thumbnail,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Container(
+            width: 120,
+            child: Text(
+              "${data.title.substring(0, min(data.title.length, 8))}${data.title.length > 8 ? "..." : ""}",
               style: TextStyle(fontSize: 22, color: Colors.orange),
             ),
-            SizedBox(
-              width: 20,
+          ),
+          SizedBox(
+            width: 50,
+          ),
+          Text(
+            "${data.upvotes}",
+            style: TextStyle(fontSize: 22, color: Colors.orange),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          IconButton(
+            iconSize: 24,
+            icon: Icon(
+              Icons.keyboard_arrow_up,
+              color: Colors.orange,
             ),
-            IconButton(
-              iconSize: 24,
-              icon: Icon(
-                Icons.keyboard_arrow_up,
-                color: Colors.orange,
-              ),
-              onPressed: () {
-                data.upvotes++;
-                onUpvoteChange();
-              },
+            onPressed: () {
+              data.upvotes++;
+              onUpvoteChange();
+            },
+          ),
+          IconButton(
+            iconSize: 24,
+            icon: Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.orange,
             ),
-            IconButton(
-              iconSize: 24,
-              icon: Icon(
-                Icons.keyboard_arrow_down,
-                color: Colors.orange,
-              ),
-              onPressed: () {
-                data.upvotes--;
-                onUpvoteChange();
-              },
-            )
-          ],
-        ),
+            onPressed: () {
+              data.upvotes--;
+              onUpvoteChange();
+            },
+          )
+        ],
       ),
     );
   }
