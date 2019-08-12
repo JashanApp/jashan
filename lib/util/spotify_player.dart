8a328714bae50c69ff8a4293b3d9254a2b78696b
Arham4 <arhamjsiddiqui@gmail.com>
2019-08-12 16:52:24 -0500

    Make queuing music playing using web sockets!

--- lib/util/spotify_player.dart
+++ lib/util/spotify_player.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:adhara_socket_io/adhara_socket_io.dart';
 import 'package:collection/collection.dart';
 import 'package:http/http.dart';
 import 'package:jashan/data/playlist_item.dart';
@@ -11,8 +12,51 @@ class SpotifyPlayer {
   Function onSongChange;
   String timerSongUri;
   Timer timer;
+  final SocketIOManager manager = SocketIOManager();
+  SocketIO socket;
 
-  SpotifyPlayer({this.user});
+  SpotifyPlayer({this.user}) {
+    initSocket();
+  }
+
+  void initSocket() async {
+    socket = await manager.createInstance(SocketOptions(
+        'https://spotify-connect-ws.herokuapp.com/connect', // todo host our own
+    ));
+    socket.on("track_end", (message) {
+      // todo have a delay
+      onSongChange();
+    });
+    socket.on("track_change", (message) {
+      print('changed');
+    });
+    socket.on("playback_paused", (message) {
+      print('paused');
+    });
+    socket.connect();
+    socket.onReconnect((data) {
+      print('reconnect');
+    });
+    socket.onReconnecting((data) {
+      print('trying to reconnect');
+    });
+    socket.onReconnectError((data) {
+      print('reconnect error $data');
+    });
+    socket.onReconnectFailed((data) {
+      print('reconnect failed');
+    });
+    socket.onConnect((data) {
+      socket.emit('initiate', [ { "accessToken": '${user.accessToken}' } ]);
+      print('initiated');
+    });
+    socket.onDisconnect((data) {
+      print('disconnected');
+    });
+    socket.onError((data) {
+      print('got an error: $data');
+    });
+  }
 
   void setOnSongChange(Function onSongChange) {
     this.onSongChange = onSongChange;
@@ -20,7 +64,8 @@ class SpotifyPlayer {
 
   void playSong(QueueList<PlaylistItem> queue) {
     PlaylistItem playlistItem = queue.removeFirst();
-    put(
+    socket.emit('play', [ { 'uris': ['${playlistItem.uri}'] } ]);
+    /*put(
       'https://api.spotify.com/v1/me/player/play',
       headers: {
         'Authorization': 'Bearer ${user.accessToken}',
@@ -30,8 +75,8 @@ class SpotifyPlayer {
       body: '{'
           '"uris": ["${playlistItem.uri}"]'
           '}',
-    );
-    timerSongUri = playlistItem.uri;
+    );*/
+   /* timerSongUri = playlistItem.uri;
     // todo flaw with this approach: can adjust song time in spotify itself and ruin this
     if (timer != null) {
       timer.cancel();
@@ -40,12 +85,12 @@ class SpotifyPlayer {
         () async {
           // todo checks to not override user choice
       //String currentSongUri = await _getCurrentSongUri();
-      /*if (currentSongUri != timerSongUri) {
+      *//*if (currentSongUri != timerSongUri) {
         print('ded :( cuz $currentSongUri vs $timerSongUri');
         return;
-      }*/
+      }*//*
       onSongChange();
-    });
+    });*/
   }
 
   Future<String> _getCurrentSongUri() async {
