c0470792d8d98b614ea0e03c521dd34a92d1cf50
Arham4 <arhamjsiddiqui@gmail.com>
2019-08-14 11:51:03 -0500

    Make playlist queue item card responsive

--- lib/widgets/playlist_queue_item_card.dart
+++ lib/widgets/playlist_queue_item_card.dart
@@ -16,75 +16,82 @@ class PlaylistQueueItemCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 3.0),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Container(
-            width: 45,
-            height: 45,
-            child: isCurrentPlaying
-                ? Container(
-                    width: 45,
-                    height: 45,
-                    color: Colors.black.withOpacity(0.5),
-                    child: Center(
-                      child: Icon(
-                        Icons.audiotrack,
-                        color: Colors.white,
-                        size: 24,
-                      ),
-                    ),
-                  )
-                : null,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(data.thumbnailUrl),
-                fit: BoxFit.cover,
+              child: Row(
+            children: <Widget>[
+              Container(
+                width: 45,
+                height: 45,
+                child: isCurrentPlaying
+                    ? Container(
+                        width: 45,
+                        height: 45,
+                        color: Colors.black.withOpacity(0.5),
+                        child: Center(
+                          child: Icon(
+                            Icons.audiotrack,
+                            color: Colors.white,
+                            size: 24,
+                          ),
+                        ),
+                      )
+                    : null,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(data.thumbnailUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-          ),
-          SizedBox(
-            width: 15,
-          ),
+              SizedBox(
+                width: 15,
+              ),
+              Container(
+                width: 120,
+                child: Text(
+                  "${data.title.substring(0, min(data.title.length, 8))}${data.title.length > 8 ? "..." : ""}",
+                  style: TextStyle(fontSize: 22, color: Colors.orange),
+                ),
+              ),
+            ],
+          )),
           Container(
-            width: 120,
-            child: Text(
-              "${data.title.substring(0, min(data.title.length, 8))}${data.title.length > 8 ? "..." : ""}",
-              style: TextStyle(fontSize: 22, color: Colors.orange),
-            ),
-          ),
-          SizedBox(
-            width: 50,
-          ),
-          Text(
-            "${data.upvotes}",
-            style: TextStyle(fontSize: 22, color: Colors.orange),
-          ),
-          SizedBox(
-            width: 20,
-          ),
-          IconButton(
-            iconSize: 24,
-            icon: Icon(
-              Icons.keyboard_arrow_up,
-              color: Colors.orange,
-            ),
-            onPressed: () {
-              data.upvotes++;
-              onUpvoteChange();
-            },
-          ),
-          IconButton(
-            iconSize: 24,
-            icon: Icon(
-              Icons.keyboard_arrow_down,
-              color: Colors.orange,
-            ),
-            onPressed: () {
-              data.upvotes--;
-              onUpvoteChange();
-            },
-          )
+              child: Row(
+            children: <Widget>[
+              Text(
+                "${data.upvotes}",
+                style: TextStyle(fontSize: 22, color: Colors.orange),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              IconButton(
+                iconSize: 24,
+                icon: Icon(
+                  Icons.keyboard_arrow_up,
+                  color: Colors.orange,
+                ),
+                onPressed: () {
+                  data.upvotes++;
+                  onUpvoteChange();
+                },
+              ),
+              IconButton(
+                iconSize: 24,
+                icon: Icon(
+                  Icons.keyboard_arrow_down,
+                  color: Colors.orange,
+                ),
+                onPressed: () {
+                  data.upvotes--;
+                  onUpvoteChange();
+                },
+              ),
+            ],
+          )),
         ],
       ),
     );
