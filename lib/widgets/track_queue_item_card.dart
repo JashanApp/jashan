d088f2e3de3105b6226b5a5b3b41e48b7874649d
Arham4 <arhamjsiddiqui@gmail.com>
2019-08-18 19:09:43 -0500

    Visual improvement to current-playing background
    Took 1 minute

--- lib/widgets/track_queue_item_card.dart
+++ lib/widgets/track_queue_item_card.dart
@@ -25,124 +25,126 @@ class TrackQueueItemCard extends StatelessWidget {
     return InkWell(
       onLongPress: onLongPress,
       child: Container(
-        color: isCurrentPlaying ? Theme.of(context).primaryColor : Colors.white,
         padding: const EdgeInsets.symmetric(vertical: 3.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: (MediaQuery.of(context).size.width - 50) * 0.65,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 50,
-                    height: 50,
-                    child: isCurrentPlaying
-                        ? Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.black.withOpacity(0.5),
-                            child: Center(
-                              child: Icon(
-                                Icons.audiotrack,
-                                color: Theme.of(context).accentColor,
-                                size: 24,
+        child: Container(
+          color: isCurrentPlaying ? Theme.of(context).primaryColor : Colors.white,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: (MediaQuery.of(context).size.width - 50) * 0.65,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 50,
+                      height: 50,
+                      child: isCurrentPlaying
+                          ? Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.black.withOpacity(0.5),
+                              child: Center(
+                                child: Icon(
+                                  Icons.audiotrack,
+                                  color: Theme.of(context).accentColor,
+                                  size: 24,
+                                ),
                               ),
-                            ),
-                          )
-                        : null,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(data.thumbnailUrl),
-                        fit: BoxFit.cover,
+                            )
+                          : null,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(data.thumbnailUrl),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    height: 45,
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          getTextWithCap(data.title, 18),
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
+                    Container(
+                      height: 45,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            getTextWithCap(data.title, 18),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: isCurrentPlaying
+                                    ? Theme.of(context).accentColor
+                                    : Colors.black,
+                                fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            getTextWithCap(data.artist, 22),
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
                               color: isCurrentPlaying
                                   ? Theme.of(context).accentColor
                                   : Colors.black,
-                              fontSize: 16),
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Text(
-                          getTextWithCap(data.artist, 22),
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            color: isCurrentPlaying
-                                ? Theme.of(context).accentColor
-                                : Colors.black,
-                            fontSize: 13,
+                              fontSize: 13,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-                width: (MediaQuery.of(context).size.width - 50) * 0.35,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "${data.getValue()}",
-                      style: TextStyle(
-                          fontSize: 22,
-                          color: isCurrentPlaying
-                              ? Theme.of(context).accentColor
-                              : upvotesHasUser || downvotesHasUser
-                              ? Theme.of(context).primaryColor
-                              : Colors.black,
-                          fontWeight: isCurrentPlaying || upvotesHasUser || downvotesHasUser
-                              ? FontWeight.bold
-                              : FontWeight.normal),
-                    ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    IconButton(
-                      iconSize: 24,
-                      icon: Icon(
-                        Icons.keyboard_arrow_up,
-                        color: isCurrentPlaying
-                            ? Theme.of(context).accentColor
-                            : upvotesHasUser
-                            ? Theme.of(context).primaryColor
-                            : Colors.black,
+                        ],
                       ),
-                      onPressed: () {
-                        onUpvoteChange(true);
-                      },
-                    ),
-                    IconButton(
-                      iconSize: 24,
-                      icon: Icon(
-                        Icons.keyboard_arrow_down,
-                        color: isCurrentPlaying
-                            ? Theme.of(context).accentColor
-                            : downvotesHasUser
-                                ? Theme.of(context).primaryColor
-                                : Colors.black,
-                      ),
-                      onPressed: () => onUpvoteChange(false),
                     ),
                   ],
-                )),
-          ],
+                ),
+              ),
+              Container(
+                  width: (MediaQuery.of(context).size.width - 50) * 0.35,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "${data.getValue()}",
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: isCurrentPlaying
+                                ? Theme.of(context).accentColor
+                                : upvotesHasUser || downvotesHasUser
+                                ? Theme.of(context).primaryColor
+                                : Colors.black,
+                            fontWeight: isCurrentPlaying || upvotesHasUser || downvotesHasUser
+                                ? FontWeight.bold
+                                : FontWeight.normal),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      IconButton(
+                        iconSize: 24,
+                        icon: Icon(
+                          Icons.keyboard_arrow_up,
+                          color: isCurrentPlaying
+                              ? Theme.of(context).accentColor
+                              : upvotesHasUser
+                              ? Theme.of(context).primaryColor
+                              : Colors.black,
+                        ),
+                        onPressed: () {
+                          onUpvoteChange(true);
+                        },
+                      ),
+                      IconButton(
+                        iconSize: 24,
+                        icon: Icon(
+                          Icons.keyboard_arrow_down,
+                          color: isCurrentPlaying
+                              ? Theme.of(context).accentColor
+                              : downvotesHasUser
+                                  ? Theme.of(context).primaryColor
+                                  : Colors.black,
+                        ),
+                        onPressed: () => onUpvoteChange(false),
+                      ),
+                    ],
+                  )),
+            ],
+          ),
         ),
       ),
     );
