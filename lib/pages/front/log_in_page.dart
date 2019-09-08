9353280bd19eb216fa86ae9298832f9be9f7e98a
Arham4 <arhamjsiddiqui@gmail.com>
2019-09-07 21:19:15 -0500

    Add HTTP error logging
    Took 52 minutes

--- lib/pages/front/log_in_page.dart
+++ lib/pages/front/log_in_page.dart
@@ -276,6 +276,9 @@ class LogInPageState extends State<LogInPage> {
         '&scope=$scope&show_dialog=$DEBUG}',
         userAgent:
             'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36');
+    flutterWebviewPlugin.onHttpError.listen((WebViewHttpError item) {
+      print("   WebView    onHttpError.code: ${item.code}");
+    });
     flutterWebviewPlugin.onUrlChanged.listen((String url) async {
       if (url.contains('?code=')) {
         final String code =
